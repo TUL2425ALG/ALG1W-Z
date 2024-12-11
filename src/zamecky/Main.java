f1542551cd3658e1c782a935af0cab6d24582847
Zamecky-T-45b4 <Zamecky-T-45b4@a2-06-mti.mshome.net>
2024-12-11 08:00:08 +0100

    create main

---
+++ src/zamecky/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zamecky;
+
+/**
+ *
+ * @author Zamecky-T-45b4
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
