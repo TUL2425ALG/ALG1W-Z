31037164d9668d3f0ae1f9279445f2c783bec4e3
Vavra-V-e4e5 <Vavra-V-e4e5@a2-19-mti.mshome.net>
2024-12-11 07:59:08 +0100

    added Main.java

---
+++ src/vavra/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vavra;
+
+/**
+ *
+ * @author Vavra-V-e4e5
+ */
+public class Main {
+    
+}
