e597abf054d32d24f20f878d71e2f187b72c0612
Smidova-Z-0dca <Smidova-Z-0dca@a2-13-mti.mshome.net>
2024-12-11 10:20:42 +0100

    final commit

--- src/smidova/Main.java
+++ src/smidova/Main.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package smidova;
 
-/**
- *
- * @author Smidova-Z-0dca
- */
 public class Main {
+
     public static void main(String[] args) {
-        
     }
-   
+
 }
---
+++ src/smidova/Marathon.java
@@ -0,0 +1,60 @@
+package smidova;
+
+import java.util.Scanner;
+
+public class Marathon {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        final double VYBEH = 9.15;
+        final double TRASADELKA = 42.195;
+        final int HOURS_TO_SECS = 3600;
+        final int MINUTES_TO_SECS = 60;
+
+        while (true) {
+
+            System.out.println("Zadej pocet bezcu: ");
+            int pocetBezcu = sc.nextInt();
+            System.out.println("Zadej jejich cislo a cas v cili (hodiny  minuty  sekundy): ");
+
+            for (int i = 0; i < pocetBezcu; i++) {
+                int cisloBezce = sc.nextInt();
+                int casBezceHodiny = sc.nextInt();
+                int casBezceMinuty = sc.nextInt();
+                int casBezceSekundy = sc.nextInt();
+                int celkovyCas = casBezceHodiny * HOURS_TO_SECS + casBezceMinuty * MINUTES_TO_SECS + casBezceSekundy;
+
+                System.out.println("Bezec cislo " + cisloBezce + "  " + casBezceHodiny + ":" + casBezceMinuty + "." + casBezceSekundy);
+                double rychlost = TRASADELKA / celkovyCas;
+                System.out.println("ma rychlost: " + rychlost);
+
+                System.out.println("");
+
+//                double max = 2.07;
+//                double pom = 0;
+//                if (rychlost < max) {
+//
+//                    System.out.println("Rychlost nejrychlejsiho bezce: " + rychlost);
+                
+                //dva nejpomalejsi: 
+                int min = 10;
+                if (rychlost < min) {
+                    min = cisloBezce;
+
+                    System.out.println("Nejpomalejsi bezec:" + cisloBezce);
+
+                }
+            }
+
+            //prumer nejrychlejsiho 
+//                if (rychlost < CAS) {
+//                    System.out.println(cisloBezce);
+            if (pocetBezcu == 0) {
+                break;
+            }
+
+            System.out.println("");
+
+        }
+    }
+}
