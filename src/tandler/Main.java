10e06973e4ace44df4b8104af6528a79809a8752
Dominik Tandler <dominik.tandler@tul.cz>
2024-12-11 07:59:42 +0100

    DT

---
+++ src/tandler/Main.java
@@ -0,0 +1,16 @@
+package tandler;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Tandler-D-0f5f
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
