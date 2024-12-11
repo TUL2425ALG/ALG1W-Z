a7a456a779b7ff49a0c93356e84ba019f37b8b0a
Krupka-M-4d24 <Krupka-M-4d24@a2-21-mti.mshome.net>
2024-12-11 10:23:07 +0100

    ulohy 1 a 2

---
+++ src/alg1/z/Marathon.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alg1.z;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Krupka-M-4d24
+ */
+public class Marathon {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadje pocet bezcu");
+        int pocet_bezcu = sc.nextInt();
+        while (pocet_bezcu != 0) {
+            int puvodni_cas = 9 * 360 + 15 * 60; //Start v 9:15
+            double delka_trati = 42.195;
+            int rychly_bezci = 0;
+            int nejpomalejsi_cas1 = 0;
+            int nejpomalejsi_bezec1 = 0;
+            int nejpomalejsi_cas2 = 1;
+            int nejpomalejsi_bezec2 = 0;
+            int nejrychlejsi_cas = nejpomalejsi_cas1;
+            double rychlost = 0;
+            for (int i = 0; i < pocet_bezcu; i++) {
+                Scanner sc1 = new Scanner(System.in);
+                System.out.println("Zadej cislo bezce a jeho cas");
+                int cislo_bezce = sc.nextInt();
+                int hodina = sc.nextInt();
+                int minuta = sc.nextInt();
+                int sekunda = sc.nextInt();
+
+                int cas = hodina * 360 + minuta * 60 + sekunda;
+
+                if (cas - puvodni_cas <= 1140) { //2*360 + 7 * 60
+                   rychly_bezci++;
+                }
+                if (cas >= nejpomalejsi_cas1) {
+                    nejpomalejsi_cas2 = nejpomalejsi_cas1;
+                    nejpomalejsi_cas1 = cas;
+                    
+                    nejpomalejsi_bezec2 = nejpomalejsi_bezec1;
+                    nejpomalejsi_bezec1 = cislo_bezce;
+                }
+                
+                if(cas < nejrychlejsi_cas){
+                    nejrychlejsi_cas = cas;
+                }
+            }
+            rychlost = ((delka_trati * 1000)/(nejrychlejsi_cas - puvodni_cas)) * 3.6; 
+        
+        }
+    }
+}
---
+++ src/alg1/z/Methods.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alg1.z;
+
+/**
+ *
+ * @author Krupka-M-4d24
+ */
+public class Methods {
+
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public static boolean arePositiveAndSumDiagonalsSame(int[][] a) {
+        a = matice;
+        int n = a.length;
+        boolean kladna = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] < 0) {
+                    kladna = false;
+                }
+            }
+            return kladna;
+        }
+        if (kladna == true) {
+            int sumaHlavni = 0;
+            for (int i = 0; i < n; i++) {
+                for (i = 0; i < n; i++) {
+                    sumaHlavni = sumaHlavni + a[i][i];
+                }
+            }
+            int sumaVedlejsi = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    sumaVedlejsi = sumaVedlejsi + a[i][n - i];
+                }
+            }
+            boolean stejne = false;
+            if (sumaHlavni == sumaVedlejsi) {
+                stejne = true;
+            }
+            return stejne;
+        }
+    }
+
+    public static void createPalindom(String[] args) {
+        int[] posloupnost = a/2;
+        int[] obracena_posloupnost = a/2;
+        int[] palindom = posloupnost + obracena_posloupnost
+        int a = posloupnost.length * 2;
+        if(posloupnost.length % 2 != 0){
+            a = a - 1;
+        }
+        for(int i = 0; i < ; i++)
+        
+        
+    }
+}
