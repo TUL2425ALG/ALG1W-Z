fc94532765620a59e809d00e4845a9bac08e1e9d
Vrana-T-183a <Vrana-T-183a@a2-07-mti.skynet.tul.cz>
2024-12-11 10:21:46 +0100

    AAAAA

---
+++ src/vrana/AppMatrix.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vrana;
+import java.util.Scanner;
+/**
+ *
+ * @author Vrana-T-183a
+ */
+public class AppMatrix {
+                    public static void main(String[] args) {
+                        // VSTUPNÍ HODNOTA NAPŘ.
+                        // 11
+                        // 11
+                        Scanner sc = new Scanner(System.in);
+                        System.out.println("Vypiš matici po řádcích.");
+                        String radek = sc.nextLine();
+                        radek.replaceAll(" ","");
+                        
+                        int[][] mat = new int[radek.length()][radek.length()];
+                        for(int i = 0; i<radek.length();i++)
+                        {
+                            mat[0][i] = radek.charAt(i);
+                        }
+                        for(int j = 1; j<radek.length(); j++)
+                        {
+                            radek = sc.nextLine();
+                            radek.replaceAll(" ","");
+                            for (int i = 0; i < radek.length(); i++)
+                            {
+                                mat[j][i] = radek.charAt(i);   
+                            }
+                        }
+                        boolean hodnota = arePositiveAndSumDiagonalsSame(mat);
+                        if(arePositiveAndSumDiagonalsSame(mat) == true)
+                        {
+                            System.out.println("Pravda!");
+                        }
+                        else
+                        {
+                            System.out.println("Nepravda!");
+                        }
+                    }
+                public static boolean arePositiveAndSumDiagonalsSame(int m[][])
+                {
+                    int soucethl=0;
+                    int soucetdiag=0;
+                    for(int i = 0; i < m.length; i++)
+                    {
+                        soucethl = soucethl + m[i][i];
+                        soucetdiag = soucetdiag + m[i][m.length-1-i];
+                        for(int j = 0; j < m.length; j++)
+                        {
+                            if(m[j][i] < 0)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    if(soucethl != soucetdiag)
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+}
