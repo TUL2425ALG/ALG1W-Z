3774f4ecea051823eb056258e58e8e9ede895890
ul <ul@a2-04-mti.mshome.net>
2024-12-11 10:21:39 +0100

    zápočet

---
+++ src/koubkova/AppMatrix.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koubkova;
+
+/**
+ *
+ * @author ul
+ */
+public class AppMatrix {
+
+    public static void main(String[] args) {
+        System.out.println("Načti matici: ");
+        int[][]matice 
+        boolean vysledek = AppMatrix(matice);
+    }
+
+}
